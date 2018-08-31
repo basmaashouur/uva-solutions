9805109c918934263bbd70ec10d4096ff1efff8a
Basma Ashour <basmaashouur@gmail.com>
2018-08-31 13:28:38 +0200

    Create 369-UVa.cpp

---
+++ v003/369-UVa.cpp
@@ -0,0 +1,33 @@
+/*
+Name: 369 - Combinations
+Online Judge:UVa
+Tags:
+Complexity:
+Tutorial: Solve using cummaltive product but in matrix
+*/
+
+
+// C O D E
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+
+
+int main()
+{
+	while (true)
+	{
+		int n, m;
+		cin >> n >> m;
+		if (n == 0 && m == 0)break;
+		for (int x = n; x > n - m; x--)
+		{
+
+
+		}
+	}
+
+	return 0;
+}
